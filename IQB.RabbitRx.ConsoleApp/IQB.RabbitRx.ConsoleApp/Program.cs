01b660c326c7d05abaaf25eaf5f7d3f373c2188d
jaco <jaco.kleynhans@icloud.com>
2020-07-28 20:36:22 +0200

    Console Apps with basic functionality

---
+++ IQB.RabbitRx.ConsoleApp/IQB.RabbitRx.ConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace IQB.RabbitRx.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receiver starting.");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            string queueName = "userQueue";
+            var rabbitMqConnection = factory.CreateConnection();
+            var rabbitMqChannel = rabbitMqConnection.CreateModel();
+
+            rabbitMqChannel.QueueDeclare(queue: queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            rabbitMqChannel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            int messageCount = Convert.ToInt16(rabbitMqChannel.MessageCount(queueName));
+            Console.WriteLine(" Listening to the queue. This channels has {0} messages on the queue", messageCount);
+
+            var consumer = new EventingBasicConsumer(rabbitMqChannel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(Regex.IsMatch(message, @"^[a-zA-Z]+$")
+                    ? $@"Hello {message}, I am your father!”"
+                    : "Unparseable input received.");
+
+                rabbitMqChannel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                Thread.Sleep(1000);
+            };
+            rabbitMqChannel.BasicConsume(queue: queueName,
+                autoAck: true ,
+                consumer: consumer);
+
+            Thread.Sleep(1000 * messageCount);
+            Console.WriteLine(" Connection closed, no more messages.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IQB.RabbitTx.ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+namespace IQB.RabbitTx.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rabbit Transmitter Starting");
+            var text = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(text))
+            {
+                Console.WriteLine("Please enter a value.");
+                Console.ReadLine();
+            }
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "userQueue",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+
+
+                var body = Encoding.UTF8.GetBytes(text);
+
+                channel.BasicPublish(exchange: "",
+                    routingKey: "userQueue",
+                    basicProperties: null,
+                    body: body);
+            }
+            Console.WriteLine("Name sent successfully");
+            Console.ReadLine();
+        }
+    }
+}
